e2aecbd989d5fe00d04e50f83334ebcf82fa8b60
koyanagi <koyanagi@bbtech.co.jp>
2024-04-25 09:00:12 +0100

    Updated to version 4.0

--- WRAP_SiTCPXG_XC7V_128K.v
+++ WRAP_SiTCPXG_XC7V_128K.v
@@ -16,6 +16,7 @@
 //			20201221	#20201221	Virtex7
 //			20210901	#20210901	Changed the core of SiTCP library to SiTCPXG_XC7V_128K_V2
 //			20221024	#20221024	Changed the core of SiTCP library to SiTCPXG_XC7V_128K_V3
+//			20240419	#20240419	Changed the core of SiTCP library to SiTCPXG_XC7V_128K_V4
 //
 //----------------------------------------------------------------------//
 module
@@ -124,7 +125,7 @@ module
 		.TIM_1S							(TIM_1S							)	// out	: 1 s interval
 	);
 
-	SiTCPXG_XC7V_128K_V3	SiTCP(			// #20221024
+	SiTCPXG_XC7V_128K_V4	SiTCP(			// #20240419
 		.REG_FPGA_VER					(REG_FPGA_VER[31:0]				),	// in	: User logic Version(For example, the synthesized date)
 		.REG_FPGA_ID					(REG_FPGA_ID[31:0]				),	// in	: User logic ID (We recommend using the lower 4 bytes of the MAC address.)
 		//		==== System I/F ====
